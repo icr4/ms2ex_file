16ac553a843e03f88e3a4918c16e8cb272295008
Alessandro Gessa <alessandro@gessa.net>
2024-07-29 12:03:27 +0200

    Atomize json keys

--- lib/ms2ex_file/structs.ex
+++ lib/ms2ex_file/structs.ex
@@ -72,7 +72,7 @@ defmodule Ms2exFile.Structs do
       |> Enum.with_index()
       |> Enum.reduce(%{}, fn {row, i}, map ->
         field = Enum.at(fields, i) |> String.to_atom()
-        Map.put(map, field, row)
+        Map.put(map, field, atomize_map_keys(row))
       end)
 
     struct = struct(struct, row)
@@ -80,5 +80,33 @@ defmodule Ms2exFile.Structs do
     id = Map.get(struct, id_key)
 
     {id, struct}
+  end
+
+  defp atomize_map_keys(map) when is_list(map) do
+    Enum.map(map, &atomize_map_keys/1)
+  end
+
+  defp atomize_map_keys(%NaiveDateTime{} = dt),
+    do: dt
+
+  defp atomize_map_keys(map) when is_map(map) do
+    Enum.map(map, fn r ->
+      case r do
+        {k, v} when is_binary(k) -> {atomize_key(k), atomize_map_keys(v)}
+        {k, v} -> {k, atomize_map_keys(v)}
+      end
+    end)
+    |> Map.new()
+  end
+
+  defp atomize_map_keys(data), do: data
+
+  defp atomize_key(key) when is_binary(key) do
+    key = field_name(key)
+
+    case Integer.parse(field_name(key)) do
+      {_, _} -> key
+      _ -> String.to_atom(key)
+    end
   end
 end
