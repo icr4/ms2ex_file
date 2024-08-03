a4c6a5ce4ce24810f03b2d9fde9661dcbb90aab4
Alessandro Gessa <alessandro@gessa.net>
2024-08-03 12:43:07 +0200

    Edit config

--- config/config.exs
+++ config/config.exs
@@ -1,13 +1,5 @@
 import Config
 
 config :ms2ex_file,
-  mysql_uri: "mysql://root:password@localhost:3306/maple_data",
-  redis_uri: "redis://localhost"
-
-# hostname: System.get_env("DB_HOST") || "admin.taakagency.it",
-# username: System.get_env("DB_USER") || "root",
-# password: System.get_env("DB_PASS") || "password",
-# database: System.get_env("DB_NAME") || "maple_data",
-# port: System.get_env("DB_PORT") || 3308
-
-# config :ms2ex_file, :redix, host: System.get_env("RD_HOST") || "localhost"
+  mysql_uri: System.get_env("MYSQL_URI") || "mysql://root:password@localhost:3306/maple-data",
+  redis_uri: System.get_env("REDIS_URI") || "redis://localhost"
